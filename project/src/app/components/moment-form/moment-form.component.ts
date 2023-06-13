a42a666c70db193cd94ddd5ec197db20c95e0fb9
Guilherme-Araujo-Dev <guilhermearaujop26@gmail.com>
2023-06-13 14:44:36 -0300

    Impedindo valores nulos em campos obrigatórios

--- project/src/app/components/moment-form/moment-form.component.ts
+++ project/src/app/components/moment-form/moment-form.component.ts
@@ -1,17 +1,34 @@
 import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
-  styleUrls: ['./moment-form.component.css']
+  styleUrls: ['./moment-form.component.css'],
 })
 export class MomentFormComponent {
   @Input() btnText!: string;
 
-  momentForm!:FormGroup
+  momentForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl(''),
+      image: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get title() {
+    return this.momentForm.get('title')!
+  }
+
+  get image() {
+    return this.momentForm.get('image')!
+  }
 
   submit() {
-    console.log("Enviando");
+    console.log('Enviando');
   }
 }
